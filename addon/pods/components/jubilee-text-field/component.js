7b5007e1b18e6c897b1be0013821cda9bda54838
ChristianEllerbrock <christian.ellerbrock@inbayern.net>
2019-02-26 23:41:40 +0100

    Implement one-way binding component with @onChange event

--- addon/pods/components/jubilee-text-field/component.js
+++ addon/pods/components/jubilee-text-field/component.js
@@ -1,9 +1,19 @@
 import Component from '@ember/component';
 import layout from './template';
 import { computed } from '@ember/object';
-import { equal } from '@ember/object/computed';
+import { equal, oneWay } from '@ember/object/computed';
 
 export default Component.extend({
+  /*
+    @label {string}
+    @label2 {string}
+    @text {string}
+    @update {string} - 'on-blur', 'on-input' (default)
+    @onChange {callback}
+    @isReadonly {boolean}
+  */
+
+
   // #region Properties
 
   layout,
@@ -14,14 +24,55 @@ export default Component.extend({
 
   computedIsReadonly: equal('isReadonly', true),
 
+  onewayText: oneWay('text'),
+
+  isOnBlur: equal('update', 'on-blur'),
+
   // #endregion Properties
 
 
   // #region Actions
 
   actions: {
-    
+    onBlur(/*event*/) {
+      if (!this.isOnBlur) {
+        // @update === 'on-input'
+        // Changes would have been propagated before. 
+        // Nothing to do here.
+        return;
+      }
+
+      this._triggerOnChange(this.onewayText);
+    },
+
+    onInput(/*event*/) {
+      if (this.isOnBlur) {
+        // Nothing to do here.
+        return;
+      }
+
+      this._triggerOnChange(this.onewayText);
+    },
   },
 
   // #endregion Actions
+
+
+  // #region Methods
+
+  _triggerOnChange(newValue) {
+    if (this.onewayText == '' && this.text == undefined) {
+      return;
+    }
+
+    if (this.onewayText === this.text) {
+      return;
+    }
+
+    if (this.onChange) {
+      this.onChange(newValue);
+    }
+  }
+
+  // #endregion Methods
 });
