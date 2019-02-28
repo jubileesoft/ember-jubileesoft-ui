8db29c2ab56a0febcc0a3a54b5196afef89d3444
ChristianEllerbrock <christian.ellerbrock@inbayern.net>
2019-02-28 22:49:29 +0100

    Implement first version of JubileeSelect

--- addon/pods/components/jubilee-select/component.js
+++ addon/pods/components/jubilee-select/component.js
@@ -1,6 +1,81 @@
 import Component from '@ember/component';
 import layout from './template';
+import { oneWay } from '@ember/object/computed';
 
 export default Component.extend({
-  layout
+  // #region Properties
+
+  layout,
+
+  onewaySelectedItem: oneWay('selectedItem'),
+
+  isDropdownOpen: false,
+
+  // #endregion Properties
+
+
+  // #region Fields
+
+  _triggerId: null, // will be set later
+  _dropdownId: null,
+
+  // #endregion Fields
+
+
+  // #region Hooks
+
+  didRender() {
+    if (!this._triggerId) {
+      this.set('_triggerId', this.elementId + '_trigger');
+    }
+
+    if (!this._dropdownId) {
+      this.set('_dropdownId', this.elementId + '_dropdown');
+    }
+
+    this._adjustDropdownWidth();
+  },
+
+  // #endregion Hook
+
+
+  // #region Actions
+
+  actions: {
+    triggerClicked() {
+      this.toggleProperty('isDropdownOpen');
+      this._adjustDropdownPosition();
+    },
+
+    itemClicked(item) {
+      if (typeof this.onChange == 'function' && this.selectedItem !== item) {
+        this.onChange(item);
+      }
+
+      this.toggleProperty('isDropdownOpen');
+      this._adjustDropdownWidth();
+    },
+  },
+
+  // #endregion Actions
+
+
+  // #region Methods
+
+  _adjustDropdownWidth() {
+    const triggerElement = document.getElementById(this._triggerId);
+    const dropdownElement = document.getElementById(this._dropdownId);
+
+    dropdownElement.style.minWidth = triggerElement.offsetWidth + 'px';
+  },
+
+  _adjustDropdownPosition() {
+    let index = this.items.indexOf(this.selectedItem);
+
+    const dropdownElement = document.getElementById(this._dropdownId);
+
+    dropdownElement.style.top = '-' + (index * 2.5 + 0.5) + 'rem';
+  },
+
+  // #endregion Methods
 });
