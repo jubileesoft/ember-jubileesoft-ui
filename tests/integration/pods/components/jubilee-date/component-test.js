8a9c379d3c209c5d4678e8a375a439674041b93a
ChristianEllerbrock <christian.ellerbrock@inbayern.net>
2019-04-15 22:17:58 +0200

    Upgrade to ember-cli@3.9.0

--- tests/integration/pods/components/jubilee-date/component-test.js
+++ tests/integration/pods/components/jubilee-date/component-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | jubilee-date', function(hooks) {
 
     await render(hbs`{{jubilee-date}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | jubilee-date', function(hooks) {
       {{/jubilee-date}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
--- tests/integration/pods/components/jubilee-text-field/component-test.js
+++ tests/integration/pods/components/jubilee-text-field/component-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | jubilee-text-field', function(hooks) {
 
     await render(hbs`{{jubilee-text-field}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | jubilee-text-field', function(hooks) {
       {{/jubilee-text-field}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
