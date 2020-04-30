c18d4979d39e28d5e2c092436330951d1917d9d5
ChristianEllerbrock <christian.ellerbrock@inbayern.net>
2020-04-30 23:12:32 +0200

    Enable theming

--- index.js
+++ index.js
@@ -12,57 +12,35 @@ module.exports = {
 
       return '<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet" type="text/css" media="all">';
     }
+  },
+
+  included(app) {
+    if (app.options.emberJubileesoftUi == undefined) {
+      app.options.emberJubileesoftUi = {};
+    }
+
+    let config = app.options.emberJubileesoftUi;
+
+    this.options = Object.assign({}, this.options, {
+      cssModules: {
+        virtualModules: {
+          // https://material.io/tools/color/#!/?view.left=1&view.right=1&primary.color=F06292&primary.text.color=ffffff&secondary.color=A9F263
+          colors: {
+            primary: config.primary || '#f06292',
+
+            accent: config.accent || '#797979',
+            accentLight: config.accentLight || '#d7d7d7',
+            accentDark: config.accentDark || '#5c5c5c',
+
+            textOnLight: config.textOnLight || '#202020',
+            textOnDark: config.textOnDark || '#ffffff',
+            backgroundOnLight: config.backgroundOnLight || '#fafafa',
+            backgroundOnDark: config.backgroundOnDark || '#2c2c2c'
+          }
+        }
+      }
+    });
+
+    this._super.included.apply(this, arguments);
   }
-
-  // included(app) {
-  //   if (app.options.emberJubileesoftUi == undefined) {
-  //     app.options.emberJubileesoftUi = {};
-  //   }
-
-  //   let config = app.options.emberJubileesoftUi;
-
-  //   this.options = Object.assign({}, this.options, {
-  //     cssModules: {
-  //       virtualModules: {
-  //         // https://material.io/tools/color/#!/?view.left=1&view.right=1&primary.color=F06292&primary.text.color=ffffff&secondary.color=A9F263
-  //         colors: {
-  //           /*
-  //             PRIMARY
-  //           */
-  //           primary: config.primary || '#f06292',
-  //           onPrimary: config.onPrimary || '#ffffff',
-
-  //           primaryLight: config.primaryLight || '#ff94c2',
-  //           primaryDark: config.primaryDark || '#ba2d65',
-
-  //           /*
-  //             SECONDARY
-  //           */
-
-  //           secondary: config.secondary || '#a9f263',
-  //           onSecondary: config.onSecondary || '#000000',
-
-  //           secondaryLight: config.secondaryLight || '#ddff95',
-  //           secondaryDark: config.secondaryDark || '#76bf30',
-
-  //           /*
-  //             DESK
-  //           */
-  //           desk: config.desk || '#f5f7f7',
-
-  //           /*
-  //             SURFACE
-  //           */
-  //           surface: config.surface || '#ffffff',
-  //           onSurface: config.onSurface || '#696d6e',
-  //           onSurfaceDark: config.onSurfaceDark || '#3e3f42',
-
-  //           separator: config.separator || 'rgba(0,0,0,0.12)'
-  //         }
-  //       }
-  //     }
-  //   });
-
-  //  super.included.apply(this, arguments);
-  //}
 };
